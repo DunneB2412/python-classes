ed9d7513f5fbeb430c4313bc783611b9cc23767c
timmist <xtimmist24x@gmail.com>
2021-12-11 15:13:46 +0000

    setup

---
+++ main.py
@@ -0,0 +1,17 @@
+# This is a sample Python script.
+
+# Press Shift+F10 to execute it or replace it with your code.
+# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    print("hellllllllllllllllllllllllo")
+    arr = [1, 2, 3, 4]
+
+    n = 12
+    x = 3
+
+    print((n % x))
+
+    #print(arr[1])
